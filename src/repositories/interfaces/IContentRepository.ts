a7c3614202103d657ea80b20f0faa53c0ddcca22
José Ricardo <jricardobgoncalves@gmail.com>
2021-12-05 18:30:34 -0300

    :sparkles: FindByUnity method

--- src/repositories/interfaces/IContentRepository.ts
+++ src/repositories/interfaces/IContentRepository.ts
@@ -5,6 +5,7 @@ import { Content } from "../../entities/Content";
 export interface IContentRepository {
   createContent(contentParams: IContentRequest): Promise<Content>;
   findAll(): Promise<Content[]>;
+  findByUnity(unityId: string): Promise<Content[]>;
   findById(id: string): Promise<Content | undefined>;
   updateById(updateFields: IContentRequest): Promise<void>;
   deleteById(id: string): Promise<DeleteResult>;
