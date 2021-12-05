d47eabefbbdf8171e70dce0fa74a545eda0aa26c
José Ricardo <jricardobgoncalves@gmail.com>
2021-12-05 18:27:06 -0300

    :sparkles: Find contents by unity

--- src/repositories/implementations/ContentRepository.ts
+++ src/repositories/implementations/ContentRepository.ts
@@ -1,6 +1,7 @@
 import { DeleteResult, EntityRepository, getCustomRepository, Repository } from "typeorm";
 import { IContentRequest } from "../../dto/IContentRequest";
 import { Content } from "../../entities/Content";
+import { Unity } from "../../entities/Unity";
 import { IContentRepository } from "../interfaces/IContentRepository";
 import { UnityRepository } from "./UnityRepository";
 
@@ -11,16 +12,23 @@ export class ContentRepository extends Repository<Content> implements IContentRe
     
     const unityRepository = getCustomRepository(UnityRepository);
     const unity = await unityRepository.findById(unityId);
-
+    
     const newContentParams = this.create({ title, description, video, index, unity });
-
+    
     const content = await this.save(newContentParams);
-
+    
     return content;
   }
   
   async findAll(): Promise<Content[]> {
     return await this.find({ relations: ['unity'] });
+  }
+  
+  async findByUnity(unityId: string): Promise<Content[]> {
+    const unityRepository = getCustomRepository(UnityRepository);
+    const unity = await unityRepository.findById(unityId);
+
+    return await this.find({ where: { unity }, relations: ['unity'] });
   }
   
   async findById(id: string): Promise<Content> {
