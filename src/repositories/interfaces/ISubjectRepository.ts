b09890e48010fec04fd9964a51c3ceccf14ff56d
José Ricardo <jricardobgoncalves@gmail.com>
2021-12-05 18:30:49 -0300

    :sparkles: CreateManySubjects method

--- src/repositories/interfaces/ISubjectRepository.ts
+++ src/repositories/interfaces/ISubjectRepository.ts
@@ -1,9 +1,11 @@
 import { DeleteResult } from "typeorm";
+import { IManySubjects } from "../../dto/IManySubjects";
 import { ISubjectRequest } from "../../dto/ISubjectRequest";
 import { Subject } from "../../entities/Subject";
 
 export interface ISubjectRepository {
   createSubject(subjectParams: ISubjectRequest): Promise<Subject>;
+  createManySubjects(subjectParams: IManySubjects): Promise<Subject[]>;
   findAll(): Promise<Subject[]>;
   findBySchoolGrade(schoolGradeId: string): Promise<Subject[]>;
   findById(id: string): Promise<Subject | undefined>;
