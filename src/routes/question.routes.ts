40864d36215520a8f94ed8ff35cc4676e42be3ee
José Ricardo <jricardobgoncalves@gmail.com>
2021-12-19 08:16:19 -0300

    :sparkles: Question routes

---
+++ src/routes/question.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import CreateQuestionController from '../controllers/question/CreateQuestionController';
+import DeleteQuestionController from '../controllers/question/DeleteQuestionController';
+import ListQuestionController from '../controllers/question/ListQuestionController';
+import ShowQuestionController from '../controllers/question/ShowQuestionController';
+import UpdateQuestionController from '../controllers/question/UpdateQuestionController';
+
+const routes = Router();
+
+routes.get('/', ListQuestionController.handle);
+routes.get('/:id', ShowQuestionController.handle);
+routes.post('/', CreateQuestionController.handle);
+routes.put('/:id', UpdateQuestionController.handle);
+routes.delete('/:id', DeleteQuestionController.handle);
+
+export default routes;
