910e404d1ce6aa23680c12a83ca04ffdeae64e3f
José Ricardo <jricardobgoncalves@gmail.com>
2022-01-13 17:26:28 -0300

    :recycle: Add function to search duel by code

--- src/repositories/interfaces/IDuelRepository.ts
+++ src/repositories/interfaces/IDuelRepository.ts
@@ -1,11 +1,12 @@
-import { DeleteResult } from "typeorm";
-import { IDuelRequest } from "../../dto/IDuelRequest";
-import { Duel } from "../../entities/Duel";
+import { DeleteResult } from 'typeorm';
+import { IDuelRequest } from '../../dto/IDuelRequest';
+import { Duel } from '../../entities/Duel';
 
 export interface IDuelRepository {
   createDuel(duelParams: IDuelRequest): Promise<Duel>;
   findAll(name?: string): Promise<Duel[]>;
   findById(id: string): Promise<Duel | undefined>;
+  findByCode(code: string): Promise<Duel[]>;
   updateById(updateFields: IDuelRequest): Promise<void>;
   deleteById(id: string): Promise<DeleteResult>;
 }
