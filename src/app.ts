c7b7371bd749563e6fc01203cdf0750ca4181276
José Ricardo <jricardobgoncalves@gmail.com>
2022-01-13 17:16:30 -0300

    :sparkles: Add duelRound, duelRounQuestion and duelQuestionAnswer routes

--- src/app.ts
+++ src/app.ts
@@ -15,20 +15,31 @@ const swaggerDocument = { ...swaggerDefinitionDocument, paths };
 
 const { PORT } = process.env;
 
-// Rotas
+/* Rotas */
+
+// Usuarios
 
 import studentRoutes from './routes/student.routes';
 import teacherRoutes from './routes/teacher.routes';
-import subjectRoutes from './routes/subject.routes';
+
+// Materiais escolares
+
 import teachingTypeRoutes from './routes/teachingtype.routes';
 import schoolGradeRoutes from './routes/schoolgrade.routes';
+import subjectRoutes from './routes/subject.routes';
 import unityRoutes from './routes/unity.routes';
 import contentRoutes from './routes/content.routes';
 import questionRoutes from './routes/question.routes';
 import alternativeRoutes from './routes/alternative.routes';
+
+// Duelos
+
 import duelRoutes from './routes/duel.routes';
+import duelRoundRoutes from './routes/duelRound.routes';
 import duelTeamRoutes from './routes/duelTeam.routes';
 import duelTeamParticipationRoutes from './routes/duelTeamParticipation.routes';
+import duelRoundQuestionRoutes from './routes/duelRoundQuestion.routes';
+import duelQuestionAnswerRoutes from './routes/duelQuestionAnswer.routes';
 
 // Aplicacao
 
@@ -42,15 +53,30 @@ class App {
   }
 
   middlewares() {
+    /* Middlewares */
+
+    // Permitir acesso a outras aplicacoes
+
     this.app.use(cors());
+
+    // Reconhece o objeto de solicitacao como JSON
+
     this.app.use(express.json());
+
+    // Permite o uso de objetos aninhados, como no JSON
+
     this.app.use(express.urlencoded({ extended: true }));
   }
 
   routes() {
-    // Entities routes
+    /* Utilizacao das rotas */
+
+    // Usuarios
+
     this.app.use('/students', studentRoutes);
     this.app.use('/teachers', teacherRoutes);
+
+    // Materiais escolares
     this.app.use('/subjects', subjectRoutes);
     this.app.use('/teachingTypes', teachingTypeRoutes);
     this.app.use('/schoolGrades', schoolGradeRoutes);
@@ -58,11 +84,18 @@ class App {
     this.app.use('/contents', contentRoutes);
     this.app.use('/questions', questionRoutes);
     this.app.use('/alternatives', alternativeRoutes);
+
+    // Duelos
+
     this.app.use('/duels', duelRoutes);
+    this.app.use('/duelRounds', duelRoundRoutes);
     this.app.use('/duelTeams', duelTeamRoutes);
     this.app.use('/duelTeamParts', duelTeamParticipationRoutes);
+    this.app.use('/duelRoundQuestions', duelRoundQuestionRoutes);
+    this.app.use('/duelQuestionAnswers', duelQuestionAnswerRoutes);
 
-    // Docs routes
+    // Documentacao
+
     this.app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
   }
 }
