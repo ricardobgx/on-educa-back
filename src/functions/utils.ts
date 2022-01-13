ff4c35531f5b3132254d0e6aa21a39ad20ca34a3
José Ricardo <jricardobgoncalves@gmail.com>
2022-01-13 17:21:54 -0300

    :sparkles: Create function to generate alphanumeric duel code

--- src/functions/utils.ts
+++ src/functions/utils.ts
@@ -1,3 +1,53 @@
 export const randInt = (min: number, max: number): number => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
+export const generateDuelCode = (codeSize: number): string => {
+  const characters = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+  ];
+  let code = '';
+
+  for (let i = 0; i < codeSize; i++) {
+    const characterIndex = randInt(0, characters.length - 1);
+
+    code += characters[characterIndex];
+  }
+
+  return code;
+};
