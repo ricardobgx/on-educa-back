c11120aeb8531ffa165d10c078731b3a714a1049
José Ricardo <jricardobgoncalves@gmail.com>
2022-01-13 17:30:35 -0300

    :recycle: Update CreateDuelService

--- src/services/duel/CreateDuelService.ts
+++ src/services/duel/CreateDuelService.ts
@@ -1,7 +1,8 @@
-import { getCustomRepository, ObjectType } from "typeorm";
-import { IDuelRequest } from "../../dto/IDuelRequest";
-import { Duel } from "../../entities/Duel";
-import { IDuelRepository } from "../../repositories/interfaces/IDuelRepository";
+import { getCustomRepository, ObjectType } from 'typeorm';
+import { IDuelRequest } from '../../dto/IDuelRequest';
+import { Duel } from '../../entities/Duel';
+import { generateDuelCode } from '../../functions/utils';
+import { IDuelRepository } from '../../repositories/interfaces/IDuelRepository';
 
 export class CreateDuelService {
   duelRepository: IDuelRepository;
@@ -11,9 +12,25 @@ export class CreateDuelService {
   }
 
   async execute(duelParams: IDuelRequest): Promise<Duel> {
-    const duelRepository = getCustomRepository(this.duelRepository as unknown as ObjectType<IDuelRepository>);
+    const duelRepository = getCustomRepository(
+      this.duelRepository as unknown as ObjectType<IDuelRepository>
+    );
 
-    const duel = await duelRepository.createDuel(duelParams);
+    let codeExists = true;
+    let code = '';
+
+    do {
+      code = generateDuelCode(6);
+      codeExists = false;
+
+      await duelRepository.findByCode(code).then((duels) => {
+        if (duels.length > 0) {
+          codeExists = true;
+        }
+      });
+    } while (codeExists);
+
+    const duel = await duelRepository.createDuel({ ...duelParams, code });
 
     return duel;
   }
